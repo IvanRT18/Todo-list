af10525f21b1005618778e481e5e669b4e7d2a2c
Iván Rodríguez <ivan18rt@outlook.com>
2023-02-25 16:54:13 -0600

    Agregacion de archivos

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));   
+
+let Items = ["Buy food", "cook food", "eat food"];
+
+app.get("/", (req, res) =>{
+    let today = new Date();
+    let day = "";
+    let option = {
+        weekday: "long",
+        year: "numeric",
+        month: "long", 
+        day: "numeric"
+    }
+
+    day = today.toLocaleDateString("en-GB", option);
+    
+    res.render("list", {kindOfDay: day, newEntries: Items});
+});
+
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    Items.push(item);
+    res.redirect("/");
+})
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
